9fc0662c43ab941f8ac443f22729c0eb752ecc88
Hassan Naseri <hassan.naseri@accenture.com>
2018-10-01 13:04:43 +0300

    move grover functions to separate file.

---
+++ quantum_functions.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+def uint4(int_or_string):
+    # Simulate 4-bit unsigned integer by masking 4 bits of numpy.unit8
+    unit8_type = np.uint8(int_or_string)
+    mask = np.uint8(2**4 - 1)
+    return(unit8_type & mask)
+
+def black_box_check(circuit, key, q):
+    # Black box oracle function to encode a winner entry in a dataset
+    # Inverts the phase of 'q' if 'key' and 'q' are matching, otherwise the 'q' remains unchanged.
+    # Note: the implementation of the oracle here is only for simulation purposes, and its details may noy have 
+    # any ptractical relevance. In practice, the oracle is generated systematically for each problem ecncoding 
+    # a function (or dataset) that cannot be inverted (or efficiently searched).
+    data_frame = pd.read_csv("fruits2.csv")
+    data_dict = data_frame.set_index('name').T.to_dict('records')[0]
+    try:
+        winner = uint4(data_dict[key])
+    except:
+        raise ValueError('Invalid key! There is no ' + key)
+    if winner&2 == 0:
+        circuit.s(q[0])
+    if winner&1 == 0:
+        circuit.s(q[1])
+    circuit.h(q[1])
+    circuit.cx(q[0], q[1])
+    circuit.h(q[1])
+    if winner&2 == 0:
+        circuit.s(q[0])
+    if winner&1 == 0:
+        circuit.s(q[1])
+
+def reflection_about_average(circuit, q):
+    # Reflection about average for amplitude amplification
+    circuit.h(q[0])
+    circuit.h(q[1])    
+    circuit.x(q[0])
+    circuit.x(q[1])
+    circuit.h(q[1])
+    circuit.cx(q[0], q[1])
+    circuit.h(q[1])
+    circuit.x(q[0])
+    circuit.x(q[1])
+    circuit.h(q[0])
+    circuit.h(q[1])
+
+
--- readme.md
+++ readme.md
@@ -1,4 +1,21 @@
 
-# Binder link
+# Simple Exercises for Quantum Computing
+This repository contains Jupyter Notebook files to start expremineting with gate-model quantum computers. The excersises are based on **qiskit** SDK, and are only tested on IBM Q quantum backends.
+
+## Let's start with a classic search example
+Run **search_classic.ipynb** to review the complexity of an unstructured search problem.
+
+## Grover's algorithm 
+The Grover's algorithm brings quadratic speedup to the problem of unstructured search.
+
+### Overall structure of the lagorithm
+### Building the oracle funtion
+### Building the rotation function
+
+# Jupyter Notebooks in Binder
+You can find the executable Jupyter Notebook files from the follwing link:
+[![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/HassanNaseri/quantum-computing-handson/master)
 https://mybinder.org/v2/gh/HassanNaseri/quantum-computing-handson/master
+
+
 
